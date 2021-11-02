8ec995a4f360e820453bb6ac32109de883fbd785
Kunal Salvi <kunalsalvi63@gmail.com>
2021-11-02 14:36:04 +0530

    Add files via upload

---
+++ Debug/Inc/GPIO/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Inc/GPIO/GPIO.c 
+
+OBJS += \
+./Inc/GPIO/GPIO.o 
+
+C_DEPS += \
+./Inc/GPIO/GPIO.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Inc/GPIO/GPIO.o: ../Inc/GPIO/GPIO.c Inc/GPIO/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Inc/GPIO/GPIO.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -mthumb -o "$@"
+
